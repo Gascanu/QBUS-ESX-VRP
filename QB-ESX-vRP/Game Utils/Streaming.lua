465fa2197094faa2319dfc4573ce5ff40f3228b7
Gascanu <gascanumystery@gmail.com>
2022-10-08 14:37:28 +0300

    Add files via upload

---
+++ QB-ESX-vRP/Game Utils/Streaming.lua
@@ -0,0 +1,149 @@
+--[[                               
+    ________ __________          _________                       
+    \_____  \\______   \         \_   ___ \  ___________   ____  
+     /  / \  \|    |  _/  ______ /    \  \/ /  _ \_  __ \_/ __ \ 
+    /   \_/.  \    |   \ /_____/ \     \___(  <_> )  | \/\  ___/ 
+    \_____\ \_/______  /          \______  /\____/|__|    \___  >
+           \__>      \/                  \/                   \/                                   
+]]--
+
+function QBCore.Functions.RequestAnimDict(animDict)
+	if HasAnimDictLoaded(animDict) then return end
+	RequestAnimDict(animDict)
+	while not HasAnimDictLoaded(animDict) do
+		Wait(0)
+	end
+end
+
+function QBCore.Functions.LoadModel(model)
+    if HasModelLoaded(model) then return end
+	RequestModel(model)
+	while not HasModelLoaded(model) do
+		Wait(0)
+	end
+end
+
+function QBCore.Functions.LoadAnimSet(animSet)
+    if HasAnimSetLoaded(animSet) then return end
+    RequestAnimSet(animSet)
+    while not HasAnimSetLoaded(animSet) do
+        Wait(0)
+    end
+end
+
+function QBCore.Functions.LoadParticleDictionary(dictionary)
+    if HasNamedPtfxAssetLoaded(dictionary) then return end
+    RequestNamedPtfxAsset(dictionary)
+    while not HasNamedPtfxAssetLoaded(dictionary) do
+        Wait(0)
+    end
+end
+
+
+--[[                               
+    ___________ _____________  ___
+    \_   _____//   _____/\   \/  /
+     |    __)_ \_____  \  \     / 
+     |        \/        \ /     \ 
+    /_______  /_______  //___/\  \
+            \/        \/       \_/                                   
+]]--
+
+
+function ESX.Streaming.RequestModel(modelHash, cb)
+	modelHash = (type(modelHash) == 'number' and modelHash or joaat(modelHash))
+
+	if not HasModelLoaded(modelHash) and IsModelInCdimage(modelHash) then
+		RequestModel(modelHash)
+
+		while not HasModelLoaded(modelHash) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function ESX.Streaming.RequestStreamedTextureDict(textureDict, cb)
+	if not HasStreamedTextureDictLoaded(textureDict) then
+		RequestStreamedTextureDict(textureDict)
+
+		while not HasStreamedTextureDictLoaded(textureDict) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function ESX.Streaming.RequestNamedPtfxAsset(assetName, cb)
+	if not HasNamedPtfxAssetLoaded(assetName) then
+		RequestNamedPtfxAsset(assetName)
+
+		while not HasNamedPtfxAssetLoaded(assetName) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function ESX.Streaming.RequestAnimSet(animSet, cb)
+	if not HasAnimSetLoaded(animSet) then
+		RequestAnimSet(animSet)
+
+		while not HasAnimSetLoaded(animSet) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function ESX.Streaming.RequestAnimDict(animDict, cb)
+	if not HasAnimDictLoaded(animDict) then
+		RequestAnimDict(animDict)
+
+		while not HasAnimDictLoaded(animDict) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+function ESX.Streaming.RequestWeaponAsset(weaponHash, cb)
+	if not HasWeaponAssetLoaded(weaponHash) then
+		RequestWeaponAsset(weaponHash)
+
+		while not HasWeaponAssetLoaded(weaponHash) do
+			Wait(0)
+		end
+	end
+
+	if cb ~= nil then
+		cb()
+	end
+end
+
+
+--[[                               
+           ____________________ 
+     ___  _\______   \______   \
+     \  \/ /|       _/|     ___/
+      \   / |    |   \|    |    
+       \_/  |____|_  /|____|    
+                   \/                                              
+]]--
+
+-- Pe vRP nu este o functie prestabilita pentru vreuna de mai sus, dar se pot lua functiile de mai sus, se vor sterge "dependentele" precum ESX.Streaming/QBCore.Functions si ramane doar function RequestAnimDict si functia "devine" standalone
